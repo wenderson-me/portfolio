5047c19fcd0924f4ba119c532e24e8df5d98183c
Wenderson <49308695+wenderson-me@users.noreply.github.com>
2025-07-03 14:22:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/", // Corrigido para raiz, compatível com Vercel
+  base: "/Portfolio",
   plugins: [
     react(),
     runtimeErrorOverlay(),
