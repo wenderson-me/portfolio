3ae7b8266b801377501c51f34c3e2bedeace9edb
wenderson <wenderson.silva@vinci-energies.net>
2025-08-24 13:24:46 +0100

    removendo o server para estruturar melhor

--- server/routes.ts
+++
@@ -1,9 +0,0 @@
-import type { Express } from "express";
-import { createServer, type Server } from "http";
-
-export async function registerRoutes(app: Express): Promise<Server> {
-
-  const httpServer = createServer(app);
-
-  return httpServer;
-}
